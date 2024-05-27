f8970c1e7575cdfc75d4c4e83c8bcc23adc0bb4c
tomason182 <elotro182@hotmail.com>
2024-05-27 18:26:26 -0300

    add: Create a ship class and add tests

--- src/ships.js
+++ src/ships.js
@@ -1,9 +1,26 @@
-class Ships {
+class Ship {
     constructor(length) {
         this.length = length;
-        this.hits = null;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+    hit() {
+        if (this.hits >= this.length) {
+            throw new Error ('Hits exceed length')
+        }
+        this.hits += 1;
+        return this.hits;
+    }
+
+    isSunk() {
+        if (this.hits >= this.length) {
+            return true;
+        }
+
+        return this.sunk;
     }
 }
 
 
-export {Ships}
+export {Ship}
--- src/ships.test.js
+++ src/ships.test.js
@@ -1,6 +1,53 @@
-import { Ships } from "./ships.js";
+import { Ship } from "./ships.js";
 
 test('should be an instance of the class', () => {
-    expect(new Ships()).toBeInstanceOf(Ships);
+    expect(new Ship()).toBeInstanceOf(Ship);
+});
+
+test('Should initialize with the correct length', () => {
+    const shipLength = 4;
+    const ship = new Ship(shipLength);
+    expect(ship.length).toBe(4);
+    expect(ship.hits).toBe(0);
+    expect(ship.sunk).toBe(false);
+})
+
+test('Should return the correct hit count', () => {
+    const ship = new Ship();
+    expect(ship.hit()).toBe(1);
+    expect(ship.hit()).toBe(2);
+})
+
+test('Should return false when not sunk', () => {
+    const ship = new Ship();
+    expect(ship.isSunk()).toBe(false);
+});
+
+test('Should return false if hit less times than its length', () => {
+    const shipLength = 3;
+    const ship = new Ship(shipLength);
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+});
+
+test('Should return true if ship is hit times the length', () => {
+    const shipLength = 3
+    const ship = new Ship(shipLength);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);    
+});
+
+test('should not exceed the number of hits beyond the length', () => {
+    const shipLength = 3;
+    const ship = new Ship(shipLength);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(() => ship.hit()).toThrow('Hits exceed length');
+    expect(ship.hits).toBe(3);
+
 })
 
