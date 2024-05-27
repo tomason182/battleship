1d5964f62352b00f9ba4cb9455a3ee1b63047a06
tomason182 <elotro182@hotmail.com>
2024-05-27 19:34:01 -0300

    add: count ship lenght when placing it

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,10 +1,18 @@
+import { Ship } from "./ships.js";
+
 class Gameboard {
     constructor(height, width) {
         this.height = height;
         this.width = width;
     }
 
-    placeShip(x, y) {
+    placeShip(x, y, length) {
+        const ship = new Ship(length);
+
+        if (x + ship.length > this.width || y > this.height) {
+            return false;
+        };
+            
         return true;
     }
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -13,7 +13,17 @@ test('Should have a height and a width', () => {
 });
 
 test('Should place ships at specific coordinates', () => {
-    const ship = new Ship(4);
     const gameboard = new Gameboard(10, 10);
     expect(gameboard.placeShip(2, 3)).toBe(true);
+});
+
+test('Should not place ships outside the gameboard', () => {
+    const gameboard = new Gameboard(10, 10);
+    expect(gameboard.placeShip(12, 12)).toBe(false);
+});
+
+test('The length of the ship should count when placing it.', () => {
+    const ship = new Ship(4);
+    const gameboard = new Gameboard(10, 10);
+    expect(gameboard.placeShip(8, 2, ship.length)).toBe(false);
 })
