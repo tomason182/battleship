8178cb889df2adc2ed74cd3182d741909e90b4bf
tomason182 <elotro182@hotmail.com>
2024-05-27 17:17:17 -0300

    add: First test and get it run

---
+++ src/ships.js
@@ -0,0 +1,9 @@
+class Ships {
+    constructor(length) {
+        this.length = length;
+        this.hits = null;
+    }
+}
+
+
+export {Ships}
---
+++ src/ships.test.js
@@ -0,0 +1,6 @@
+import { Ships } from "./ships.js";
+
+test('should be an instance of the class', () => {
+    expect(new Ships()).toBeInstanceOf(Ships);
+})
+
