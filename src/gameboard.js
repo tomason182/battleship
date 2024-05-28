75ae3d97adfda8515863c2d0a32f31b53b36e36d
tomason182 <elotro182@hotmail.com>
2024-05-28 13:57:24 -0300

    add: build board with properties

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,7 +4,14 @@ class Gameboard {
     constructor(height = 20, width = 20) {
         this.height = height;
         this.width = width;
-        this.spaceFilled = []
+        this.board = [];
+
+        for (let i = 0; i < this.width; i++) {
+            this.board[i] = [];
+            for (let j = 0; j  < this.length; j ++) {
+                this.board[i].push({hasShip: false, receiveAttack: false});
+            }
+        }
     }
 
     placeShip(ship = new Ship()) {
