e6d4d15a754ab45639553970b00bfe19738e6282
tomason182 <elotro182@hotmail.com>
2024-05-29 18:48:37 -0300

    add ship object with name and lenght properties

--- src/game-controller.js
+++ src/game-controller.js
@@ -4,44 +4,58 @@ import { default as GameBoard } from "./gameboard.js";
 
 function gameController(playerOneName = "Player One", PlayerTwoName = "Computer") {
 
-    const ships = {
-        carrier: new Ship(5),
-        battleShip: new Ship(4),
-        cruiser: new Ship(3),
-        submarine: new Ship(3),
-        destroyer: new Ship(2)        
+  const ships = [
+    {
+      name: "carrier",
+      length: 5
+    },
+		{
+			name: "battleShip",
+			length: 4
+		},
+		{
+			name: "cruiser",
+			length: 3
+		},
+		{
+			name: "submarine",
+			length: 3
+		},
+		{
+			name: "destroyer",
+			length: 2
+		}
+  ];
+
+  const players = [
+  	{
+  	  player: new Player(playerOneName),
+      board: new GameBoard()
+  	},
+    {
+      player: new Player(PlayerTwoName),
+      board: new GameBoard()
     }
+  ];
 
-    const players = [
-        {
-            player: new Player(playerOneName),
-            board: new GameBoard()
-        },
-        {
-            player: new Player(PlayerTwoName),
-            board: new GameBoard()
-        }
-    ];
+  let activePlayer = players[0];
 
-    let activePlayer = players[0];
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  }
 
-    const switchPlayerTurn = () => {
-        activePlayer = activePlayer === players[0] ? players[1] : players[0];
-    }
+  const getActivePlayer = () => activePlayer;
 
-    const getActivePlayer = () => activePlayer;
+  const placePlayerShip = (playerIndex, ship, x, y) => {
+    return players[playerIndex].board.placeShip(ship, x, y);
+  }
 
-    const placePlayerShip = (playerIndex, ship, x, y) => {
-        return players[playerIndex].board.placeShip(ship, x, y);
-    }
+  const attack = (attackerIndex, x, y) => {
+  	const opponentIndex = attackerIndex === 0 ? 1 : 0;
+    return players[opponentIndex].board.receiveAttack(x, y);
+  }
 
-    const attack = (attackerIndex, x, y) => {
-        const opponentIndex = attackerIndex === 0 ? 1 : 0;
-        return players[opponentIndex].board.receiveAttack(x, y);
-    }
-
-    const checkForVictory = (playerIndex) => {
-        return players[playerIndex].board.allShipsSunk();
-    }
-
+  const checkForVictory = (playerIndex) => {
+    return players[playerIndex].board.allShipsSunk();
+  }
 }
