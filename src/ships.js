3808f135caafc00fac5430bcea4c13f56a0a53da
tomason182 <elotro182@hotmail.com>
2024-05-29 15:06:24 -0300

    fix: Export default Ship class

--- src/ships.js
+++ src/ships.js
@@ -1,4 +1,4 @@
-class Ship {
+export default class Ship {
     constructor(length) {
         this.length = length;
         this.hits = 0;
@@ -25,6 +25,3 @@ class Ship {
         return this.sunk;
     }
 }
-
-
-export {Ship}
