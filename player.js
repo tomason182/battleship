eb3d8c9785d67499416f709bb558fc4b69b2c9a4
tomason182 <elotro182@hotmail.com>
2024-05-29 12:57:48 -0300

    add getGameboard method
    Each player has his own gameboard

--- player.js
+++ player.js
@@ -1,6 +1,13 @@
+import { Gameboard } from './gameboard.js';
+
 class Player {
     constructor(name) {
         this.name = name;
+        this.gameboard = new Gameboard();
+    }
+
+    getGameboard() {
+        return this.gameboard;
     }
 }
 
