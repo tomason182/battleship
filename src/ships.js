60da175f2dbea552058535ce6b84de305f60f2e9
tomason182 <elotro182@hotmail.com>
2024-05-29 14:54:52 -0300

    remove positioning on ship class

--- src/ships.js
+++ src/ships.js
@@ -1,23 +1,8 @@
 class Ship {
-    constructor(length, x, y) {
+    constructor(length) {
         this.length = length;
-        this._x = x;
-        this._y = y;
         this.hits = 0;
         this.sunk = false;
-    }
-
-    setPosition(x, y) {
-        this._x = x;
-        this._y = y;
-    }
-
-    getPositionX() {
-        return this._x;
-    }
-
-    getPositionY() {
-        return this._y;
     }
 
     getHits() {
